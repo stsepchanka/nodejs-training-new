9b35dc4c16239d5f68b9c5bfba7d37e969d390f6
Tatsiana Stsepchanka <tstsepchanka@slb.com>
2020-04-06 19:09:26 +0300

    feat: dependent deletion

--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,4 +1,5 @@
-const boardsRepo = require('./board.memory.repositiory');
+const boardsRepo = require('./board.memory.repository');
+const tasksRepo = require('./../tasks/task.memory.repository');
 const Board = require('./board.model');
 const Column = require('./columns/column.model');
 
@@ -16,6 +17,14 @@ const addOne = async data => {
 
 const updateOne = (id, board) => boardsRepo.updateOne(id, board);
 
-const deleteById = async boardId => boardsRepo.deleteById(boardId);
+const deleteById = async boardId => {
+  await boardsRepo.deleteById(boardId);
+  const tasks = await tasksRepo.getAll(boardId);
+  if (tasks) {
+    await Promise.all(
+      tasks.map(task => tasksRepo.deleteById(boardId, task.id))
+    );
+  }
+};
 
 module.exports = { getAll, getById, addOne, updateOne, deleteById };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,4 +1,6 @@
 const usersRepo = require('./user.memory.repository');
+const boardsRepo = require('./../boards/board.memory.repository');
+const tasksRepo = require('./../tasks/task.memory.repository');
 const User = require('./user.model');
 
 const getAll = () => usersRepo.getAll();
@@ -13,6 +15,23 @@ const addOne = async data => {
 
 const updateOne = (id, user) => usersRepo.updateOne(id, user);
 
-const deleteById = async id => usersRepo.deleteById(id);
+const deleteById = async id => {
+  await usersRepo.deleteById(id);
+  const boards = await boardsRepo.getAll();
+  if (boards) {
+    boards.forEach(async board => {
+      const tasks = await tasksRepo.getAll(board.id);
+      if (tasks) {
+        await Promise.all(
+          tasks
+            .filter(task => task.userId === id)
+            .map(task =>
+              tasksRepo.updateOne(board.id, task.id, { ...task, userId: null })
+            )
+        );
+      }
+    });
+  }
+};
 
 module.exports = { getAll, getById, addOne, updateOne, deleteById };
